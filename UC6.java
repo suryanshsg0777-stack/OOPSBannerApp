986220ed67e32764e8f0743579e74dcf334d1f10
Suryansh <suryansh.sg.0777@gmail.com>
2026-03-03 02:24:24 +0530

    Added UC6

---
+++ UC6.java
@@ -0,0 +1,51 @@
+public class UC6 {
+
+    public static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    
+    public static String[] getP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    public static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
+
+        
+        for (int i = 0; i < 7; i++) {
+            System.out.println(O[i] + "  " + O[i] + "  " + P[i] + "  " + S[i]);
+        }
+    }
+}
